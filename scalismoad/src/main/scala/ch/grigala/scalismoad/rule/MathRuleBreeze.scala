c50529e215c0c2f0a01ed92b666c1e75821565e3
George Grigalashvili <grigalaa@gmail.com>
2021-05-23 22:55:13 +0200

    stash

--- scalismoad/src/main/scala/ch/grigala/scalismoad/rule/MathRuleBreeze.scala
+++ scalismoad/src/main/scala/ch/grigala/scalismoad/rule/MathRuleBreeze.scala
@@ -2,4 +2,63 @@ package ch.grigala.scalismoad.rule
 
 trait MathRuleBreeze[U[_], V[_], T] extends ValueRuleBreeze[U, V, T] {
 
+
+    // element-wise sin(DenseMatrix[Double])
+    def sin1(v: U[T]): U[T]
+
+    def sin2(v: V[T]): V[T]
+
+    def sin3(v: T): T
+
+    def cos1(v: U[T]): U[T]
+
+    def cos2(v: V[T]): V[T]
+
+    def cos3(v: T): T
+
+    def tan1(v: U[T]): U[T]
+
+    def tan2(v: V[T]): V[T]
+
+    def tan3(v: T): T
+
+
+    def asin1(v: U[T]): U[T]
+
+    def asin2(v: V[T]): V[T]
+
+    def asin3(v: T): T
+
+
+    def acos1(v: U[T]): U[T]
+
+    def acos2(v: V[T]): V[T]
+
+    def acos3(v: T): T
+
+
+    def atan1(v: U[T]): U[T]
+
+    def atan2(v: V[T]): V[T]
+
+    def atan3(v: T): T
+
+
+    def sinh1(v: U[T]): U[T]
+
+    def sinh2(v: V[T]): V[T]
+
+    def sinh3(v: T): T
+
+    def cosh1(v: U[T]): U[T]
+
+    def cosh2(v: V[T]): V[T]
+
+    def cosh3(v: T): T
+
+    def tanh1(v: U[T]): U[T]
+
+    def tanh2(v: V[T]): V[T]
+
+    def tanh3(v: T): T
 }
--- scalismoad/src/main/scala/ch/grigala/scalismoad/rule/ValueRuleBreeze.scala
+++ scalismoad/src/main/scala/ch/grigala/scalismoad/rule/ValueRuleBreeze.scala
@@ -1,7 +1,96 @@
 package ch.grigala.scalismoad.rule
 
+import ch.grigala.scalismoad.value.{ContainerValue, NonContainerValue, Value}
+
 // for interactions between (DenseMatrix[x], DenseVector[x], x=Double)
-trait ValueRuleBreeze[U[_], V[_], T] {
+trait ValueRuleBreeze[M[_], V[_], T] {
 
+    //    final def zero(shape: Value[_, T]): Value[_, T] = shape match {
+    //        case _: NonContainerValue[U, T] => zero
+    //        case s1: ContainerValue[U, T] => zeroS1(s1.data)
+    //        case s2: ContainerValue[V, T] => zeroS2(s2.data)
+    //    }
 
+    //    // DenseMatrix[Double] -> M[T]
+    //    final def zero(shape: M[T]): Value[M, T] = toValue(zeroS1(shape))
+    //    final def toValue(v: M[T])(implicit e: DummyImplicit): Value[M, T] = ContainerValue[M, T](v)
+    //    def zeroS1(shape: M[T]): M[T]
+    //    def oneS1(shape: M[T]): M[T]
+    //
+    //    // DenseVector[Double] -> V[T]
+    //    final def zero(shape: V[T]): Value[V, T] = toValue(zeroS2(shape))
+    //    final def toValue(v: V[T])(implicit e: DummyImplicit): Value[V, T] = ContainerValue[V, T](v)
+    //    def zeroS2(shape: V[T]): V[T]
+    //    def oneS2(shape: V[T]): V[T]
+    //
+    //    // Double
+    //    final def zero: Value[M, T] = toValue(zeroM)
+    //    final def toValue(v: T): Value[M, T] = NonContainerValue[M, T](v)
+    //    def zeroM: T
+    //    def oneM: T
+
+    // + operation for every combination of DenseMatrix, DenseVector, Double
+    def addMM(l: M[T], r: M[T]): M[T]
+
+    def addMV(l: M[T], r: V[T]): M[T]
+
+    def addMD(l: M[T], r: T): M[T]
+
+    def addVV(l: V[T], r: V[T]): V[T]
+
+    def addVD(l: V[T], r: T): V[T]
+
+    def addDM(l: T, r: M[T]): M[T]
+
+    def addDV(l: T, r: V[T]): V[T]
+
+    def addDD(l: T, r: T): T
+
+    def subMM(l: M[T], r: M[T]): M[T]
+
+    def subMV(l: M[T], r: V[T]): M[T]
+
+    def subMD(l: M[T], r: T): M[T]
+
+    def subVV(l: V[T], r: V[T]): V[T]
+
+    def subVD(l: V[T], r: T): V[T]
+
+    def subDM(l: T, r: M[T]): M[T]
+
+    def subDV(l: T, r: V[T]): V[T]
+
+    def subDD(l: T, r: T): T
+
+    def mulMM(l: M[T], r: M[T]): M[T]
+
+    def mulMV(l: M[T], r: V[T]): M[T]
+
+    def mulMD(l: M[T], r: T): M[T]
+
+    def mulVV(l: V[T], r: V[T]): V[T]
+
+    def mulVD(l: V[T], r: T): V[T]
+
+    def mulDM(l: T, r: M[T]): M[T]
+
+    def mulDV(l: T, r: V[T]): V[T]
+
+    def mulDD(l: T, r: T): T
+
+    def divMM(l: M[T], r: M[T]): M[T]
+
+    def divMV(l: M[T], r: V[T]): M[T]
+
+    def divMD(l: M[T], r: T): M[T]
+
+    def divVV(l: V[T], r: V[T]): V[T]
+
+    def divVD(l: V[T], r: T): V[T]
+
+    def divDM(l: T, r: M[T]): M[T]
+
+    def divDV(l: T, r: V[T]): V[T]
+
+    def divDD(l: T, r: T): T
 }
